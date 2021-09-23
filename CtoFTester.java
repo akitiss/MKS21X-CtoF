fa37a4a81e310f7bbfa82fa77b6b854e8a94a7b6
April Li <april.li0427@gmail.com>
2021-09-23 19:52:19 -0400

    added main function

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,9 +1,11 @@
 //they should both have one int parameter. one will have celcius parameter to change to fahrenheit and the other will be the other way around. they can be double, but i am only inputting integers, but the output will be double.
-
 public class CtoFTester{
-
   public static void main(String[] args) {
+    System.out.print("0 degrees celcius to fahrenheit: ");
+    System.out.println(celciusToFahrenheit(0));
 
+    System.out.print("0 degrees fahrenheit to celcius: ");
+    System.out.println(fahrenheitToCelsius(0));
   }
 
   public static double celciusToFahrenheit(int celcius) {
@@ -13,5 +15,4 @@ public class CtoFTester{
   public static double fahrenheitToCelsius(int fahrenheit) {
     return ((fahrenheit - 32) * 5/9);
   }
-
 }
